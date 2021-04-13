2cacc1be38267592e52818e6e2cea6d735b04825
Jan <janpfister@t-online.de>
2021-04-14 01:21:47 +0200

    add config_template.py

---
+++ config_template.py
@@ -0,0 +1,17 @@
+# File has to be named to "config.py"
+
+token = ""  # String: Discord Bot Token
+database = "lichesssublist.db"  # alternativer Name möglich
+serverid = 123 # Int: Discord Server ID
+channel_log_id = 123  # Int: Log Channel ID
+channel_clean_available = [123, 132]  # List: Int: Channel ID
+
+# Rollen IDs
+role1 = ""  # bspw. Subscriber
+role2 = ""  # bspw. Patreon
+mod = ""  # Vorraussetzung für bestimmte Befehle
+
+team = ""  # Lichess Team ID
+
+# Link zum Einladen des Bots auf den Discord Server:
+# https://discord.com/oauth2/authorize?client_id={bot-id}&scope=bot&permissions=76864
